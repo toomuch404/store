b206babd89120f4ce4f356ff3233123f82f2c518
toomuch <73192719+toomuch404@users.noreply.github.com>
2021-01-03 23:18:19 +0800

    Update filename

--- server1-local.js
+++ server1-local.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const app = require("./server/server1.js");
+const app = require("./server/server.js");
 
 app.listen(process.env.PORT || 4242, () =>
-  console.log(`Node server listening at http://localhost:${4242}!`)
+  console.log(`Node server listening at http://localhost:4242!`)
 );
